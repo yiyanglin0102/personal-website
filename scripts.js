5cac125ac2ca141d4d831c96a7205693656d85fa
yiyanglin0102 <yiyanglin0102@gmail.com>
2023-04-22 19:52:05 -0400

    Added read more button and its styles

--- scripts.js
+++ scripts.js
@@ -95,3 +95,12 @@
     });
 
 })(jQuery);
+
+function toggleIntro() {
+    var intro = document.getElementById('intro');
+    if (intro.style.display === 'none') {
+        intro.style.display = 'block';
+    } else {
+        intro.style.display = 'none';
+    }
+}
